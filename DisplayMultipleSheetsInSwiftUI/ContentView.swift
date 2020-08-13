26b9811fedf5543f03667596c848fdaa410961c5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-13 10:02:35 +0300

    init

--- DisplayMultipleSheetsInSwiftUI/ContentView.swift
+++ DisplayMultipleSheetsInSwiftUI/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+enum ActiveSheet {
+    case first
+    case second
+}
+
 struct ContentView: View {
+    
+    //@State private var showFirstSheet: Bool = false
+    //@State private var showSecondSheet: Bool = false
+    
+    @State private var isPresented: Bool = false
+    @State private var activeSheet: ActiveSheet = .first
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button(action: {
+                    self.isPresented = true
+                    self.activeSheet = .first
+                }, label: {
+                    Text("Show First Sheet")
+                })
+                
+                Button(action: {
+                    self.isPresented = true
+                    self.activeSheet = .second
+                }, label: {
+                    Text("Show Second Sheet")
+                })
+            }
+            .sheet(isPresented: $isPresented) {
+                if self.activeSheet == .first {
+                    Text("First Sheet")
+                } else if self.activeSheet == .second {
+                    Text("Second Sheet")
+                }
+            }
+            .navigationTitle("Multiple Sheets")
+        }
+        
     }
 }
 
