638133ded7f4d7adef3d89dc7d428a13e8c9714e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-13 09:47:58 +0300

    Initial Commit

---
+++ DisplayMultipleSheetsInSwiftUI/DisplayMultipleSheetsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayMultipleSheetsInSwiftUIApp.swift
+//  DisplayMultipleSheetsInSwiftUI
+//
+//  Created by ramil on 13.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisplayMultipleSheetsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
